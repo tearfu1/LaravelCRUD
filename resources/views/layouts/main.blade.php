09d8bdc0d36e9bea14637d0a7ae26b66784d744b
tearfu1 <axuior12@gmail.com>
2024-08-19 21:20:12 +0200

    added bootstrap support modified layout main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,19 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-D-sv12UV.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-H1p8gvK4.js') }}">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <nav>
-        <ul>
-            <li><a href="{{ route('main.index') }}">Main</a></li>
-            <li><a href="{{ route('about.index') }}">About</a></li>
-            <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            <li><a href="{{ route('post.index') }}">Posts</a></li>
-        </ul>
-    </nav>
+<div class="container">
+    <div class="row">
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('main.index') }}">Main</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    @yield('content')
 </div>
-@yield('content')
 </body>
 </html>
