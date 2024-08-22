b94fcc3f4f5b48a52cbcae43d742ca70673f41cd
tearfu1 <axuior12@gmail.com>
2024-08-22 21:56:50 +0200

    added layout to use Guzzle

---
+++ app/Console/Commands/ImportJsonPlaceholderCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use App\Models\Post;
+use Illuminate\Console\Command;
+
+class ImportJsonPlaceholderCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:jsonplaceholder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get data from jsonPlaceholder';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $import = new ImportDataClient();
+        $response = $import->client->request('GET', 'posts');
+        $data = json_decode($response->getBody()->getContents());
+        foreach ($data as $item) {
+            Post::firstOrCreate([
+                'title' => $item->title
+            ], [
+                'title' => $item->title,
+                'content' => $item->body,
+            ]);
+        }
+        dd('done');
+    }
+}
